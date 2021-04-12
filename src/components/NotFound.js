d2de94d63175bbcc636c757fa77e4b14e0da5cc5
Lucas <lzalaquett@uc.cl>
2021-04-11 20:46:30 -0400

    .

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,5 +1,11 @@
 import React from 'react'
+import HomeNavbar from './HomeNavbar'
 
-const NotFound = () => <h1>Error 404, Page not found</h1>
+const NotFound = () => (
+    <div>
+        <HomeNavbar />
+        <h1>Error 404, Page not found</h1>
+    </div>)
+
 
 export default NotFound
